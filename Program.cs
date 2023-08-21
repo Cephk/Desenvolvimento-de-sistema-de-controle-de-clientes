dc998456c62a2484bcc005166e50bf1a3a5afb8e
Cephk <fredericoliveiram@gmail.com>
2023-08-21 19:07:45 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Atividade;
+
+float val_pag;
+Console.WriteLine("Informar Nome");
+string var_nome = Console.ReadLine();
+Console.WriteLine("Informar Endereco");
+string var_endereco = Console.ReadLine();
+Console.WriteLine("Pessoa Física (f) ou Jurídica (j) ?");
+string var_tipo = Console.ReadLine();
+if (var_tipo == "f")
+{
+    // --- Pessoa Fisica ---
+    Pessoa_Fisica pf = new Pessoa_Fisica();
+    pf.nome = var_nome;
+    pf.endereco = var_endereco;
+    Console.WriteLine("Informa cpf");
+    pf.cpf = Console.ReadLine();
+    Console.WriteLine("Informa rg");
+    pf.rg = Console.ReadLine();
+    Console.WriteLine("Informa Valor de Compra:");
+    val_pag = float.Parse(Console.ReadLine());
+    pf.Pagar_Imposto(val_pag);
+    Console.WriteLine("-------- Pessoa Fisica --------");
+    Console.WriteLine("Nome ..........:" + pf.nome);
+    Console.WriteLine("Endereco ......:" + pf.endereco);
+    Console.WriteLine("CPF ..........:" + pf.cpf);
+    Console.WriteLine("RG ..........:" + pf.rg);
+    Console.WriteLine("Valor de Compra: " + pf.valor.ToString("C"));
+    Console.WriteLine("Imposto......:" + pf.valor_imposto.ToString("C"));
+    Console.WriteLine("Total a Pagar : " + pf.total.ToString("C"));
+}
+if (var_tipo == "j")
+{
+    // Pessoa Juridica
+    Pessoa_Juridica pj = new Pessoa_Juridica();
+    pj.nome = var_nome;
+    pj.endereco = var_endereco;
+    Console.WriteLine("Informa CNPJ");
+    pj.cnpj = Console.ReadLine();
+    Console.WriteLine("Informar IE");
+    pj.ie = Console.ReadLine();
+    Console.WriteLine("Informar Valor de Compra:");
+    val_pag = float.Parse(Console.ReadLine());
+    pj.Pagar_Imposto(val_pag);
+    Console.WriteLine("-------- Pessoa Juridica --------");
+    Console.WriteLine("Nome ........: " + pj.nome);
+    Console.WriteLine("Endereço ......: "+ pj.endereco);
+    Console.WriteLine("CNPJ ..........: "+ pj.cnpj);
+    Console.WriteLine("IE ............: "+ pj.ie);
+    Console.WriteLine("Valor de Compra: " + pj.valor.ToString("C"));
+    Console.WriteLine("Imposto......:" + pj.valor_imposto.ToString("C"));
+    Console.WriteLine("Total a Pagar : " + pj.total.ToString("C"));
+}
